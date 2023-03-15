81b960a09d5b7891bce8e78260af37a491f58f88
Abhishek Kumar <111852047+Zostelook@users.noreply.github.com>
2023-03-15 11:42:30 +0530

    Update PrimeNumber.java

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -30,3 +30,37 @@ public class PrimeNumber {
 
     }
 }
+
+
+
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    static boolean isPrime(int n){
+
+        if(n<=1){
+            return false;
+        }
+
+        if(n==2){
+            return true;
+        }
+        
+        for(int i=2; i<n/2; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+       return true;
+    }
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+
+        System.out.println(isPrime(n));
+    }
+}
